e189bbc85dc6763a7d5295a7882a7ad64f723061
alex massaroni <alex.m@seeweb.it>
2024-11-29 14:29:40 +0100

    Add event serialization method to models
    Introduced a `serialize` method in the `models.py` file to convert event instances to dictionaries, facilitating easier data handling and transmission. Updated the application version from 1.0.6 to 1.0.7 to reflect these enhancements. Updated the version number in the README and pyproject.toml accordingly.

--- src/loghero/models.py
+++ src/loghero/models.py
@@ -61,3 +61,23 @@ class Log(models.Model):
     def __str__(self):
         return f"{self.created_at.isoformat()} [{self.severity}] Actor: '{self.actor}' execute '{self.action}' on/by {self.target}, status: {self.status}, from {self.location}"
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "actor": self.actor,
+            "action": self.action,
+            "severity": self.severity,
+            "status": self.status,
+            "target_type": self.target_type,
+            "target": self.target,
+            "created_at": self.created_at.isoformat(),
+            "ip_address": self.ip_address,
+            "user_agent": self.user_agent,
+            "url": self.url,
+            "location": self.location,
+            "result_type": self.result_type,
+            "result": self.result,
+            "description": self.description,
+            "extra_data": self.extra_data,
+        }
+
